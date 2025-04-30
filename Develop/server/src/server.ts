78f1d069849b21dee7b167ca5597e7c030a01ed3
Fortunate122 <davidsaldana122@gmail.com>
2025-04-30 10:07:47 -0700

    minor file path change in server.ts

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -43,10 +43,10 @@ async function startApolloServer() {
   app.use(express.json());
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../../client/build')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
 
